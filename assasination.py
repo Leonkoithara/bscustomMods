e3f9a47de105eb7e27c3b9cc99b6362a0d917461
Leonkoithara <leon.koithara@gmail.com>
2019-09-29 01:54:41 +0530

    Changed maps and added standard powerupdrops

--- assasination.py
+++ assasination.py
@@ -8,7 +8,7 @@ def bsGetGames():
     return [Assasination]
 
 def bsGetLevels():
-    return [bs.Level('Assasination', displayName='${GAME}', gameType=testGame, settings={}, previewTexName='cragCastlePreview')]
+    return [bs.Level('Assasination', displayName='${GAME}', gameType=testGame, settings={}, previewTexName='bridgitPreview')]
 
 class testGame(bs.TeamGameActivity):
     @classmethod
@@ -21,7 +21,7 @@ class testGame(bs.TeamGameActivity):
 
     @classmethod
     def getSupportedMaps(cls, sessionType):
-        return ['Crag Castle']
+        return ['Bridgit']
 
     @classmethod
     def supportsSessionType(cls, sessionType):
@@ -42,18 +42,24 @@ class testGame(bs.TeamGameActivity):
                                 bs.NinjaBot,
                                 bs.MelBot,
                                 bs.PirateBot]
-        self._botSpawnProbs = [ 0.2,
-                                0.225,
-                                0.25,
-                                0.45,
-                                0.5,
-                                0.55,
-                                0.6,
-                                0.7,
-                                0.75,
-                                0.85,
-                                0.95,
-                                1.0]
+        self._botProb = [   0.25,
+                            0.05,
+                            0.05,
+                            0.25,
+                            0.05,
+                            0.05,
+                            0.05,
+                            0.1,
+                            0.02,
+                            0.02,
+                            0.1,
+                            0.01]
+
+        self._botSpawnProbs = []
+        psum = 0
+        for i in self._botProb:
+            psum += i
+            self._botSpawnProbs.append(psum)
 
     def onBegin(self):
         bs.TeamGameActivity.onBegin(self)
@@ -61,27 +67,40 @@ class testGame(bs.TeamGameActivity):
         self._wave = 0;
 
         self._bots = bs.BotSet()
+        self.setupStandardPowerupDrops()
 
-        bs.gameTimer(3500, bs.WeakCall(self.spawnBots), repeat=True)
+        bs.gameTimer(3500, bs.WeakCall(self.spawnBots))
+        #bs.gameTimer(2000, bs.WeakCall(self.dropPowerup))
 
     def handleMessage(self, m):
         if isinstance(m, bs.PlayerSpazDeathMessage):
             self.endGame()
 
     def spawnBots(self):
-        botSpawnPoints = [(5, 6, -2), (6, 10, -5.5), (-3, 10, -5.5), (-5, 6, -2)]
+        botSpawnPoints = [(5.5, 3, -1.5), (-5.5, 3, -1.5)]
         r = random.random()
         for i in range(len(self._botSpawnTypes)):
             if(r < self._botSpawnProbs[i]):
                 break
-
-        p1 = random.randint(0, 3)
-
+        p1 = random.randint(0, 1)
         self._bots.spawnBot(self._botSpawnTypes[i], pos=botSpawnPoints[p1])
 
-    def endGame(self):
-        #self._timer.stop()
+        r = random.random()
+        for i in range(len(self._botSpawnTypes)):
+            if(r < self._botSpawnProbs[i]):
+                break
+        p2 = random.randint(0, 1)
+        self._bots.spawnBot(self._botSpawnTypes[i], pos=botSpawnPoints[p2])
 
+        bs.gameTimer(5500, bs.WeakCall(self.spawnBots), repeat=False)
+
+    def dropPowerup(self):
+        powerupSpawnPt = (5.5, 3, -1.5)
+        powerupType = bs.Powerup.getFactory().getRandomPowerupType()
+        bs.screenMessage(powerupType)
+        bs.Powerup(position=powerupSpawnPt, powerupType=powerupType).autoRetain()
+
+    def endGame(self):
         results = bs.TeamGameResults()
 
         elapsedTime = bs.getGameTime()
